aa4ecb1a7f41d22d6c40b3c31cb6bb92f8401391
Mika Bessel <mikabessel@googlemail.com>
2019-02-07 15:48:26 +0200

    test socket rooms

--- server.js
+++ server.js
@@ -14,11 +14,18 @@ app.route('/').get((req, res) => {
 io.on('connection', (socket) => {
     console.log("User has connected:", socket.id);
 
+    socket.on('room', (room) => {
+        socket.join(room);
+        io.to(room).emit('emit', room);
+    });
+
     socket.on('disconnect', () => {
         console.log("User has disconnected:", socket.id);
     });
+
+    socket.emit('emit', 'Just a message to all Clients');
 });
 
 server.listen(5000, () => {
     console.log("Server stated!\nListening prot: 5000");
-})
+});
