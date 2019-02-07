66e98cda3a21c42669acb8d243fd25b8f4e2cfeb
Mika Bessel <mikabessel@googlemail.com>
2019-02-07 11:36:17 +0200

    connect and disconnect listener

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.route('/').get((req, res) => {
     res.send("ChatProject-Server is running!");
 });
 
-app.listen(5000, () => {
+io.on('connection', (socket) => {
+    console.log("User has connected:", socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("User has disconnected:", socket.id);
+    });
+});
+
+server.listen(5000, () => {
     console.log("Server stated!\nListening prot: 5000");
 })
